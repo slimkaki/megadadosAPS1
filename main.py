e5c54d48bbfff10274982d039b9af8306d07bf59
slimkaki <rafaalmada98@gmail.com>
2020-09-09 13:55:37 -0300

    post tarefa e get all tarefas

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Tarefa(BaseModel):
+    idTarefa: int
+    description: str
+    isDone: bool
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+db = []
 
+@app.post("/tarefa/")
+async def create_tarefa(new_tarefa: Tarefa):
+    db.append(new_tarefa)
+    return new_tarefa
 
+@app.get("/tarefas/")
+async def get_tarefas():
+    return db
