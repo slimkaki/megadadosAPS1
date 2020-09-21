734cbd7438f6b345d2864cd32e86048575434161
slimkaki <rafaalmada98@gmail.com>
2020-09-21 18:56:49 -0300

    mais alguns testes

--- test_main.py
+++ test_main.py
@@ -1,38 +1,82 @@
 from fastapi.testclient import TestClient
 from main import app
-from main import Task
-import uuid
-import json
+import json, uuid
 
 client = TestClient(app)
 
-def test_read_main_returns_not_found():
- response = client.get('/')
- assert response.status_code == 404
- assert response.json() == {'detail': 'Not Found'}
+uuids = []
 
-def test_get_all_notes():
+def test_read_main_returns_not_found():
+    """
+    Teste: teste padrão do /
+    Verbo: GET
+    """
+    response = client.get('/')
+    assert response.status_code == 404
+    assert response.json() == {'detail': 'Not Found'}
+
+def test_post_tasks_200():
+    """
+    Teste: Realizar o POST de algumas tasks e verificar se são bem-sucedidas. 
+           Os UUID's são guardados para os próximos testes.
+    Verbo: POST
+    """
+    for i in range(3):
+        data = json.dumps({
+            "description": f"meu Teste {i}",
+            "completed": "false"
+            })
+        response = client.post('/task', data)
+        uuids.append(response.json())
+
+        assert response.status_code == 200
+
+def test_post_tasks_404():
+    """
+    Teste: Realizar 
+    Verbo: POST 
+    """
+    for i in range(3):
+        data = json.dumps({
+            "maca": f"Teste {i}",
+            "banana": "false"
+            })
+        response = client.post('/task', data)
+        uuids.append(response.json())
+        assert response.status_code == 404
+
+def test_get_all():
+    """
+    Teste:
+    Verbo: GET
+    Saída esperada:
+    """
     response = client.get('/task')
     assert response.status_code == 200
+    for j in response.json():
+        if j in uuids:
+            assert True
+        else:
+            assert False
 
-def test_create_note_ok():
-    # Should return 200 due to correct payload
-    data = json.dumps(
-        {"description": "test_one", "completed": "false"}
-    )
-    response = client.post(
-        "/task",
-        data
-    )
-    print(response.json())
+def test_get_single_task():
+    """
+    Teste: retorna uma task só. É passado um UUID conhecido
+    Verbo: GET
+    Esperado: Status Code = 200
+    """
+    url = "/task/" + str(uuids[0])
+    response = client.get(url)
     assert response.status_code == 200
 
-def test_create_note_not_ok():
-    # Should return 422 due to incorrect payload
-    data = json.dumps(
-    {"incorrect": "payload", "test": "false"}
-    )
-    response = client.post(
-        "/task", data)
+def test_get_single_task_wrong_uuid():
+    """
+    Teste: retorna uma task só, porém é passado um UUID aleatório
+    Verbo: GET
+    Esperado: Status Code = 404
+    """
+    url = "/task/" + str(uuid.UUID)
+    response = client.get(url)
     assert response.status_code == 422
 
+# def test_put_replace():
